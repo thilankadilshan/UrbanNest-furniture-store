88e2b9a93a4f51ce77a3a0225b93f42ac8a87de0
thilankadilshan <thilanka.cv@gmail.com>
2025-05-07 03:08:54 +0530

    footer section component created

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,41 +1,62 @@
-import React from 'react'
-import './Footer.css'
-import footer_logo from '../Assets/logo.png'
-import intagram_icon from '../Assets/instagram_icon.png'
-import pintester_icon from '../Assets/pintester_icon.png'
-import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import React from "react";
+import "./Footer.css";
+import footerLogo from "../Assets/logo.png";
+import instagramIcon from "../Assets/instagram_icon.png";
+import pinterestIcon from "../Assets/pintester_icon.png";
+import whatsappIcon from "../Assets/whatsapp_icon.png";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className="footer-logo">
-        <img src={footer_logo} alt="" />
-        <p>Gift Store</p>
-      </div>
-      <ul className="footer-links">
-        <ul>Company</ul>
-        <ul>Products</ul>
-        <ul>Offices</ul>
-        <ul>About</ul>
-        <ul>Contact</ul>
-      </ul>
-      <div className="footer-social-item">
-        <div className="footer-icons-container">
-            <img src={intagram_icon} alt="" />
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-logo-section">
+          <img src={footerLogo} alt="Brand Logo" className="footer-logo" />
+          <p>Your trusted source for elegant, lasting furniture.</p>
         </div>
-        <div className="footer-icons-container">
-            <img src={pintester_icon} alt="" />
-        </div>
-        <div className="footer-icons-container">
-            <img src={whatsapp_icon} alt="" />
-        </div>
-      </div>
-      <div className="footer-copyright">
-        <hr />
-        <p>Copright @2024 - All Right reserved</p>
-      </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className="footer-links-section">
+          <h4>Quick Links</h4>
+          <ul className="footer-links">
+            <li>
+              <a href="#">Company</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Stores</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-social-section">
+          <h4>Follow Us</h4>
+          <div className="footer-icons">
+            <a href="#">
+              <img src={instagramIcon} alt="Instagram" />
+            </a>
+            <a href="#">
+              <img src={pinterestIcon} alt="Pinterest" />
+            </a>
+            <a href="#">
+              <img src={whatsappIcon} alt="WhatsApp" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <hr />
+        <p>&copy; 2024 TracktorPartsLk. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
