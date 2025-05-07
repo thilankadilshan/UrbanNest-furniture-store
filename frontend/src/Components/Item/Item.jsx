76f80036d8d15839852db28c89a98dbdf599a878
thilankadilshan <thilanka.cv@gmail.com>
2025-05-07 20:24:08 +0530

    item file updated Changes to be committed: 	modified:   frontend/src/Components/Item/Item.jsx

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './Item.css';
-import { Link } from 'react-router-dom'; 
+import React from "react";
+import "./Item.css";
+import { Link } from "react-router-dom";
 
 const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt={props.name} />
+        <img
+          onClick={() => window.scrollTo(0, 0)}
+          src={props.image}
+          alt={props.name}
+        />
         <p>{props.name}</p>
         <div className="item-prices">
           <div className="item-price-new">Rs: {props.new_price}</div>
