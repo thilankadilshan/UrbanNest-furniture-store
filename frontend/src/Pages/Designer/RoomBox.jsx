b9315a33d0a5ae518718dd902f10b5e91848eaae
thilankadilshan <thilanka.cv@gmail.com>
2025-05-07 21:45:46 +0530

    3d model room setup Changes to be committed: 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/public/models/chair.glb 	new file:   frontend/public/models/sofa.glb 	new file:   frontend/public/models/table.glb 	modified:   frontend/src/Pages/Designer/Furniture.jsx 	modified:   frontend/src/Pages/Designer/RoomBox.jsx 	modified:   frontend/src/Pages/Designer/RoomPreview.jsx

--- frontend/src/Pages/Designer/RoomBox.jsx
+++ frontend/src/Pages/Designer/RoomBox.jsx
@@ -1,13 +1,13 @@
-// // src/Pages/Designer/RoomBox.jsx
-// import React from "react";
+// src/Pages/Designer/RoomBox.jsx
+import React from "react";
 
-// const RoomBox = ({ width, height, length, color }) => {
-//   return (
-//     <mesh>
-//       <boxGeometry args={[width, height, length]} />
-//       <meshStandardMaterial color={color} wireframe={false} />
-//     </mesh>
-//   );
-// };
+const RoomBox = ({ width, height, length, color }) => {
+  return (
+    <mesh>
+      <boxGeometry args={[width, height, length]} />
+      <meshStandardMaterial color={color} wireframe={false} />
+    </mesh>
+  );
+};
 
-// export default RoomBox;
+export default RoomBox;
--- frontend/src/Pages/Designer/RoomPreview.jsx
+++ frontend/src/Pages/Designer/RoomPreview.jsx
@@ -1,31 +1,31 @@
-// // src/Pages/Designer/RoomPreview.jsx
-// import React from "react";
-// import { useLocation } from "react-router-dom";
-// import { Canvas } from "@react-three/fiber";
-// import { OrbitControls } from "@react-three/drei";
-// import RoomBox from "./RoomBox";
-// import Furniture from "./Furniture";
+// src/Pages/Designer/RoomPreview.jsx
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import RoomBox from "./RoomBox";
+import Furniture from "./Furniture";
 
-// export const RoomPreview = () => {
-//   const { state } = useLocation();
-//   const { width, height, length, color, selectedFurniture } = state;
+export const RoomPreview = () => {
+  const { state } = useLocation();
+  const { width, height, length, color, selectedFurniture } = state;
 
-//   return (
-//     <div style={{ height: "100vh", background: "#eee" }}>
-//       <Canvas camera={{ position: [10, 10, 10], fov: 45 }}>
-//         <ambientLight intensity={0.5} />
-//         <directionalLight position={[5, 10, 5]} intensity={1} />
-//         <OrbitControls />
-//         <RoomBox
-//           width={parseFloat(width)}
-//           height={parseFloat(height)}
-//           length={parseFloat(length)}
-//           color={color}
-//         />
-//         {selectedFurniture.map((item, i) => (
-//           <Furniture key={i} type={item} position={[i * 2 - 2, 0, 0]} />
-//         ))}
-//       </Canvas>
-//     </div>
-//   );
-// };
+  return (
+    <div style={{ height: "100vh", background: "#eee" }}>
+      <Canvas camera={{ position: [10, 10, 10], fov: 45 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 10, 5]} intensity={1} />
+        <OrbitControls />
+        <RoomBox
+          width={parseFloat(width)}
+          height={parseFloat(height)}
+          length={parseFloat(length)}
+          color={color}
+        />
+        {selectedFurniture.map((item, i) => (
+          <Furniture key={i} type={item} position={[i * 2 - 2, 0, 0]} />
+        ))}
+      </Canvas>
+    </div>
+  );
+};
